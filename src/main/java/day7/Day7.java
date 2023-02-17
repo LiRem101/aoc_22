b926e6fe45da65e534cb9b9c26894bf4ff6db1c6
Lian Remme <lirem101@hhu.de>
2023-02-17 22:27:03 +0100

    Solved Part 2 of Day 7

--- src/main/java/day7/Day7.java
+++ src/main/java/day7/Day7.java
@@ -3,10 +3,12 @@ package day7;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashSet;
 import java.util.List;
 
 public class Day7 {
+  private static final int TOTAL_DISK_SPACE = 70000000;
+  private static final int NEEDED_DISK_SPACE = 30000000;
+
   public static Directory getDirectoryStructure(String[] dirsAsString) {
     DirectoryCreator creator = new DirectoryCreator();
     for(String console : dirsAsString) {
@@ -29,11 +31,28 @@ public class Day7 {
         .sum();
   }
 
+  public static int getSpaceToBeFreed(Directory root) {
+    int takenSpace = root.getSize();
+    return NEEDED_DISK_SPACE - (TOTAL_DISK_SPACE - takenSpace);
+  }
+
+  public static int sizeOfSmallestToDelete(String[] dirsAsString) {
+    Directory root = getDirectoryStructure(dirsAsString);
+    int minSize = getSpaceToBeFreed(root);
+    List<Directory> dirs = root.getDirs();
+    return dirs.stream()
+        .mapToInt(Directory::getSize)
+        .filter(x -> x >= minSize)
+        .min()
+        .orElseThrow();
+  }
+
   public static void main(String[] args) throws IOException {
     Path path = Path.of("src/main/resources/day7Input1");
     List<String> contentList = Files.readAllLines(path);
     String[] content = contentList.toArray(new String[0]);
     System.out.println(countSmallDirs(content));
+    System.out.println(sizeOfSmallestToDelete(content));
   }
 
 }
--- src/test/java/day7/Day7Test.java
+++ src/test/java/day7/Day7Test.java
@@ -2,11 +2,14 @@ package day7;
 
 import static day7.Day7.countSmallDirs;
 import static day7.Day7.getDirectoryStructure;
+import static day7.Day7.getSpaceToBeFreed;
+import static day7.Day7.sizeOfSmallestToDelete;
 
 
-import java.util.ArrayList;
 import java.util.List;
 import junit.framework.TestCase;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class Day7Test extends TestCase {
 
@@ -74,6 +77,46 @@ public class Day7Test extends TestCase {
     String[] contents = contentsComplete.split("\n");
     int result = countSmallDirs(contents);
     assertEquals(95437, result);
+  }
+
+  public void testHowMuchSpaceNeeded() {
+    MockitoAnnotations.openMocks(this);
+    Directory mockRoot = Mockito.mock(Directory.class);
+    Mockito.when(mockRoot.getSize()).thenReturn(48381165);
+    int result = getSpaceToBeFreed(mockRoot);
+    assertEquals(8381165, result);
+  }
+
+  public void testSmallestDirToDelete()
+  {
+    String contentsComplete = """
+        $ cd /
+        $ ls
+        dir a
+        14848514 b.txt
+        8504156 c.dat
+        dir d
+        $ cd a
+        $ ls
+        dir e
+        29116 f
+        2557 g
+        62596 h.lst
+        $ cd e
+        $ ls
+        584 i
+        $ cd ..
+        $ cd ..
+        $ cd d
+        $ ls
+        4060174 j
+        8033020 d.log
+        5626152 d.ext
+        7214296 k
+        """;
+    String[] contents = contentsComplete.split("\n");
+    int result = sizeOfSmallestToDelete(contents);
+    assertEquals(24933642, result);
   }
 
 }
